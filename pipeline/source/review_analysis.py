ee57f589ec7972287cc79be60790c86f237cd1d2
Zhe Wang <zhe.wang@vanderbilt.edu>
2017-08-01 12:02:24 -0400

    Update review_analysis.py

--- pipeline/source/review_analysis.py
+++ pipeline/source/review_analysis.py
@@ -13,5 +13,6 @@ load_tableDatabase(spark, 'Category')
 
 df = spark.sql("SELECT features, categoryName from Category where categoryName='laptops'")
 df.show()
-print df.features
+for i in df.collect:
+  print i.features
 
