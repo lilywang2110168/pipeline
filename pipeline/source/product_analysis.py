149a4391c9c5b82cd534368bc91c49d1852fea9e
Zhe Wang <zhe.wang@vanderbilt.edu>
2017-08-01 15:21:11 -0400

    Update product_analysis.py

--- pipeline/source/product_analysis.py
+++ pipeline/source/product_analysis.py
@@ -1,1 +1,19 @@
 
+from spark import (get_sc, load_tableDatabase, load_tableISCDM)
+from multiprocessing import Pool
+import pyspark
+import os
+import time
+import json
+import depparse, sentiment,extract
+import spacy
+
+
+sc = get_sc()
+spark = pyspark.sql.SparkSession(sc)
+
+load_tableDatabase(spark, 'ReviewAnalysis_features')
+load_tableDatabase(spark, 'ReviewAnalysis')
+
+df= spark.sql("SELECT product, reviewId from ReviewAnalysis GROUP BY product")
+df.show()
