740aef79ced73548746c674b022e287636173bb2
Zhe Wang <zhe.wang@vanderbilt.edu>
2017-08-01 11:57:45 -0400

    Update review_analysis.py

--- pipeline/source/review_analysis.py
+++ pipeline/source/review_analysis.py
@@ -1,5 +1,5 @@
 
-from spark import (get_sc, load_table)
+from spark import (get_sc, load_tableDatabase, load_tableISCDM)
 import pyspark
 import os
 import time
@@ -8,9 +8,9 @@ import json
 sc = get_sc()
 spark = pyspark.sql.SparkSession(sc)
 
-load_table(spark, 'LilyLaptopReviews', 'ISC.DM.{}')
-load_table(spark, 'Category', 'Database.{}')
+load_tableISCDM(spark, 'LilyLaptopReviews')
+load_tableDatabase(spark, 'Category')
 
-df = spark.sql("SELECT features, categoryName from Category")
+df = sparkDatabase.sql("SELECT features, categoryName from Category")
 df.show()
 
