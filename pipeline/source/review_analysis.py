60c720fc22433a24e584239cf0b326d3b9102464
Zhe Wang <zhe.wang@vanderbilt.edu>
2017-08-01 11:07:11 -0400

    Update review_analysis.py

--- pipeline/source/review_analysis.py
+++ pipeline/source/review_analysis.py
@@ -1,1 +1,10 @@
 
+import pyspark
+
+from spark import (get_sc, load_table)
+
+sc = get_sc()
+spark = pyspark.sql.SparkSession(sc)
+load_table(spark, 'Category')
+df = spark.sql('SELECT one from natetest')
+df.show()
