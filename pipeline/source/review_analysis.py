190b8a77013bece87e9df4556823ee79caddbca7
Zhe Wang <zhe.wang@vanderbilt.edu>
2017-08-01 11:58:25 -0400

    Update review_analysis.py

--- pipeline/source/review_analysis.py
+++ pipeline/source/review_analysis.py
@@ -11,6 +11,6 @@ spark = pyspark.sql.SparkSession(sc)
 load_tableISCDM(spark, 'LilyLaptopReviews')
 load_tableDatabase(spark, 'Category')
 
-df = sparkDatabase.sql("SELECT features, categoryName from Category")
+df = spark.sql("SELECT features, categoryName from Category")
 df.show()
 
