b6872ea88de1a6120a5d64aeb0a695cab983ebb0
Zhe Wang <zhe.wang@vanderbilt.edu>
2017-08-01 13:29:00 -0400

    Update review_analysis.py

--- pipeline/source/review_analysis.py
+++ pipeline/source/review_analysis.py
@@ -13,11 +13,13 @@ spark = pyspark.sql.SparkSession(sc)
 load_tableISCDM(spark, 'LilyLaptopReviews')
 load_tableDatabase(spark, 'Category')
 
+'''
 df = spark.sql("SELECT features, categoryName from Category where categoryName='laptops'")
 df.show()
 for i in df.collect():
   print i.features
   print type(i.features)
+'''
 
 nltk_feats=['size', 'screen resolution', 'number pad', 'desktop replacement', 'hard drive', 'touch screen', 'speed', 'port', 'wireless mouse', 'build quality', 'sound quality', 'desktop', 'machine', 'window', 'program', 'speaker', 'power cord', 'screen size', 'power button', 'backlit keyboard', 'customer service', 'word processing', 'video card', 'graphic', 'operating system', 'button', 'tech support', 'battery life', 'light weight', 'optical drive', 'mouse pad', 'software']
 
