c327dce52b183453eac69f9416fa7e22eec8ae05
Zhe Wang <zhe.wang@vanderbilt.edu>
2017-08-01 11:17:11 -0400

    Update review_analysis.py

--- pipeline/source/review_analysis.py
+++ pipeline/source/review_analysis.py
@@ -1,1 +1,12 @@
 
+from spark import (get_sc, load_table)
+import pyspark
+import os
+import time
+import json
+
+sc = get_sc()
+spark = pyspark.sql.SparkSession(sc)
+
+load_table(spark, 'LilyLaptopReviews')
+df = spark.sql('SELECT reviewText from LilyLaptopReviews')
